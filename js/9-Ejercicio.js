27fe024ffb8b00d655fb35d331a8fcacac92cd72
Juan Barrionuevo <jottabarrionuevo@gmail.com>
2023-03-06 09:36:06 -0300

    se completaron los ejercicios a excepción del 9 y el Readme

--- js/9-Ejercicio.js
+++
@@ -1,8 +0,0 @@
-let text = prompt("Escribe una frase");
-let nText = text.length;
-let i;
-for (i = 0; i < nText; i++) {
-if (text.charAt(i,1) === "a" || text.charAt(i,1) === "e" || text.charAt(i,1) === "i" || text.charAt(i,1) === "o" || text.charAt(i,1) === "u") {
-document.write(text.charAt(i,1);
-}
-}
